99ed6eee522aadf1da5c1faa551de03643b2e834
yasindu10 <yasindusamarasinghe10@gmail.com>
2024-07-05 07:58:43 +0530

    remove windows platform and add some pages

--- lib/pages/camera_page.dart
+++ lib/pages/camera_page.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({
@@ -16,6 +17,38 @@ class CameraPage extends StatefulWidget {
 class _CameraPageState extends State<CameraPage> {
   @override
   Widget build(BuildContext context) {
-    return CameraPreview(widget.controller);
+    final size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        SizedBox(
+          width: size.width,
+          height: size.height,
+          child: FittedBox(
+            fit: BoxFit.cover,
+            child: SizedBox(
+              width: 100,
+              child: CameraPreview(widget.controller),
+            ),
+          ),
+        ),
+        const Gap(20),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(50, 0, 0, 0),
+              shape: BoxShape.circle,
+            ),
+            child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: const Icon(
+                  Icons.camera_rear,
+                  color: Colors.white,
+                  size: 30,
+                )),
+          ),
+        )
+      ],
+    );
   }
 }
