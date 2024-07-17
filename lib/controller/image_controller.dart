da7e0ad595ad6278b1fa0b887ad114eff4552020
Yasindu Samarasinghe <94032631+yasindu10@users.noreply.github.com>
2024-07-17 20:26:41 +0530

    Update image_controller.dart

--- lib/controller/image_controller.dart
+++ lib/controller/image_controller.dart
@@ -49,7 +49,7 @@ Future<String?> cropImage(String imagePath, BuildContext context) async {
 
 Future<String> uploadImage(File? imageFile) async {
   String uploadUrl =
-      'https://gemini-math-project-production.up.railway.app/api/v1/ai/';
+      'your server request url';
 
   var request = http.MultipartRequest('GET', Uri.parse(uploadUrl));
   request.files
